39feb2278b9bc0a1c988e64f7f11cbb6abbd8e71
Juan Carlos Hernandez <you@example.com>
2022-04-02 18:32:14 -0500

    HU-038 Completada informacion de resumen con iconos

--- src/pages/components/UserStats.js
+++ src/pages/components/UserStats.js
@@ -1,7 +1,10 @@
+import { AddBox, Star } from "@mui/icons-material";
 import { Box, Dialog, Grid, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import GameContext from "../../context/game-context";
+import solIcon from "../../assets/sol.png"
+import monedaIcon from "../../assets/moneda.png"
 import SnackBarContext from "../../context/snack-bar-context";
 
 const UserStats = (props) => {
@@ -20,22 +23,46 @@ const UserStats = (props) => {
         }
     }, []);
 
+    const styles = {
+        typography: {
+            fontSize: 18,
+            color: '#FFF',
+            fontWeight: 600,
+            position: "relative",
+            top: "-42px",
+            right: "-20px"
+
+        }
+    };
+
     return (
         <React.Fragment>
-            <Grid container justifyContent='space-between' alignItems='center'>
+            <Grid container spacing={3} justifyContent='space-between' alignItems='center'>
                 <Grid item xs={4}>
-                    <div style={{ backgroundColor: '#D8D8D8', borderRadius: '10px', height: '40px', width: '80%', display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
-                        133
+                    <div style={{ backgroundColor: '#D8D8D8', borderRadius: '10px', height: '45px', width: '90%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingRight: '20px', paddingLeft: '4px' }}>
+                        <div >
+                            <Star style={{ color: '#4285F4', fontSize: "50px", marginTop: '35px' }}></Star>
+                            <Typography style={styles.typography}>{props.resume ? props.resume.level : '-'}</Typography>
+                        </div>
+                        <Typography style={{ fontSize: '30px', fontWeight: '600' }}> {props.resume ? props.resume.currentExperience : '-'}</Typography>
+                        <div></div>
+
+
+
                     </div>
                 </Grid>
                 <Grid item xs={4}>
-                    <div style={{ backgroundColor: '#D8D8D8', borderRadius: '10px', height: '40px', width: '80%', display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
-                        8
+                    <div style={{ backgroundColor: '#D8D8D8', borderRadius: '10px', height: '45px', width: '90%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingRight: '3px', paddingLeft: '4px' }}>
+                        <AddBox style={{ color: '#34A853', fontSize: "55px", marginLeft:'-15px' }}></AddBox>
+                        <Typography style={{ fontSize: '30px', fontWeight: '600' }}> {props.resume ? props.resume.notesOwned : '-'}</Typography>
+                        <img width='30px' src={solIcon} />
                     </div>
                 </Grid>
                 <Grid item xs={4}>
-                    <div style={{ backgroundColor: '#D8D8D8', borderRadius: '10px', height: '40px', width: '80%', display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
-                      {props.resume ? props.resume.coinsOwned : '--'}
+                    <div style={{ backgroundColor: '#D8D8D8', borderRadius: '10px', height: '45px', width: '90%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingRight: '3px', paddingLeft: '4px' }}>
+                        <AddBox style={{ color: '#34A853', fontSize: "55px", marginLeft:'-15px' }}></AddBox>
+                        <Typography style={{ fontSize: '30px', fontWeight: '600' }}> {props.resume ? props.resume.coinsOwned : '-'}</Typography>
+                        <img width='30px' src={monedaIcon} />
                     </div>
                 </Grid>
             </Grid>
