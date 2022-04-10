c58d895c88bf43cf5c101622c283ec3a8a53dfd3
Juan Carlos Hernandez <you@example.com>
2022-04-10 14:36:34 -0500

    HU-015 Implementacion de modal de calibracion

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,12 +1,19 @@
-import { Paper, Typography } from "@mui/material";
+import { CircularProgress, Dialog, Paper, Typography } from "@mui/material";
 import React from "react";
 import piano from '../assets/piano.png';
 import viola from '../assets/viola.png';
 import chello from '../assets/chello.png';
 import oboe from '../assets/oboe.png';
 import arpa from '../assets/arpa.png';
+import calibracion from '../assets/calibracion.png';
 import { PauseRounded } from "@mui/icons-material";
 import { Box } from "@mui/system";
+
+import '@tensorflow/tfjs'
+import * as tmPose from '@teachablemachine/pose'
+
+let buttonStyle = { width: '150px', height: '50px', borderRadius: '15px', mx: '40px', backgroundColor: 'secondary.main', display: 'flex', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold', mt: '20px' };
+
 
 const Game = () => {
 
@@ -66,15 +73,105 @@ const Game = () => {
         }
     }
 
+    const URL = "https://teachablemachine.withgoogle.com/models/F6a7piIOE/";
+    let model, webcam, ctx, labelContainer, maxPredictions;
+    const [open, setOpen] = React.useState(true);
+    const [loading, setLoading] = React.useState(true);
+
+    const init = async () => {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // Note: the pose library adds a tmPose object to your window (window.tmPose)
+        model = await tmPose.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const size = 400;
+        const flip = true; // whether to flip the webcam
+        webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append/get elements to the DOM
+        const canvas = document.getElementById("canvas");
+        canvas.width = size; canvas.height = size;
+        ctx = canvas.getContext("2d");
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+        setLoading(false);
+    }
+
+    const loop = async () => {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+
+    const predict = async () => {
+        // Prediction #1: run input through posenet
+        // estimatePose can take in an image, video or canvas html element
+        const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+        // Prediction 2: run input through teachable machine classification model
+        const prediction = await model.predict(posenetOutput);
+
+        for (let i = 0; i < maxPredictions; i++) {
+            const classPrediction =
+                prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            labelContainer.childNodes[i].innerHTML = classPrediction;
+        }
+
+        // finally draw the poses
+        drawPose(pose);
+    }
+
+    const drawPose = async (pose) => {
+        if (webcam.canvas) {
+            ctx.drawImage(webcam.canvas, 0, 0);
+            // draw the keypoints and skeleton
+            if (pose) {
+                const minPartConfidence = 0.5;
+                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+            }
+        }
+    }
+
+
 
 
     return (
         <React.Fragment>
-            <Paper square={true} sx={{ backgroundColor: 'primary.light', height: '100%', width: '100%', padding: '15px' }}>
+            <Paper square={true} sx={{ backgroundColor: 'primary.light', height: '100%', width: '100%', padding:'20px' }}>
+                <Dialog PaperProps={{ style: { borderRadius: '50px', backgroundColor: '#FFED66', padding: '30px', display: 'flex', alignItems: 'center' } }} open={open} onClose={() => { setOpen(false) }} fullWidth maxWidth="lg">
+
+                    <div style={{ width: '100%', marginBottom:'20px' }}>
+                        <Typography fontWeight='600' fontSize="30px!important"> Calibra tu cuerpo con la cámara web:</Typography>
+                    </div>
+
+                    {loading ? <Box className="hover" sx={buttonStyle} onClick={() => { init() }}>
+                        <Typography className="title-button" fontSize="30px!important" > Iniciar</Typography>
+                    </Box> : ''}
+                    {loading && <CircularProgress style={{marginTop:'30px'}}></CircularProgress>}
+
+                    <canvas style={{ height: '300px!important', width: '300px!important' }} id="canvas"></canvas>
+                    {!loading && <img style={{ position: 'absolute', height: '400px', width: '400px', top: '95px' }} src={calibracion} />}
+                    <div id="label-container"></div>
+                    {!loading ? <Box className="hover" sx={buttonStyle} onClick={() => { setOpen(false) }}>
+                        <Typography className="title-button" fontSize="30px!important" > Realizado</Typography>
+                    </Box> : ''}
+                </Dialog>
+
                 <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography fontWeight='600' fontSize='30px' style={{ flex: 1, display: 'flex' }}> Ahora tocando: Symphony No. 9</Typography>
                     <Typography color='secondary' fontSize="30px" style={{ flex: 1, display: 'flex', justifyContent: 'center' }}> 0% Completado</Typography>
-                    <div style={{flex:1, display: 'flex', justifyContent:'flex-end'}}>
+                    <div style={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
                         <Box className="hover" style={{ backgroundColor: '#FF5E5B', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px', width: '50px' }}>
                             <PauseRounded style={{ color: '#FFF', fontSize: '50px' }} />
                         </Box>
