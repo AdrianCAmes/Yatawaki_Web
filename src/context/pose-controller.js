39ed27ca9f0d1a8869ae7dce77c49b2597ad65d2
Juan Carlos Hernandez <you@example.com>
2022-04-26 20:05:42 -0500

    Mejorada validacion de checkPunzada

--- src/context/pose-controller.js
+++ src/context/pose-controller.js
@@ -1,7 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
 
 const PoseContext = createContext({
-    checkPlumada: null
+    checkPunzada: null
 })
 
 export default PoseContext;
@@ -9,56 +9,50 @@ export default PoseContext;
 export const PoseContextProvider = (props) => {
 
     let time = 0;
-    let arrayVerificarPlumada = ['', '', ''];
-    let arraySegundosVerificarPlumada = ['', '', ''];
+    let arrayVerificarPunzada = ['', '', ''];
+    let arraySegundosVerificarPunzada = ['', '', ''];
     let timerOn = true
 
-    const checkPlumada = (value) => {
+    const checkPunzada = (value) => {
         let timeString = ("0" + Math.floor((time / 60000) % 60)).slice(-2) + ":" + ("0" + Math.floor((time / 1000) % 60)).slice(-2) + ":" + ("0" + ((time / 10) % 100)).slice(-2)
 
         //verificamos movimientos
-        if (arrayVerificarPlumada[0] == 'LHD' && arrayVerificarPlumada[1] == 'LHU' && arrayVerificarPlumada[2] == 'LHD') {
+        if (arrayVerificarPunzada[0] == 'LHD' && arrayVerificarPunzada[1] == 'LHU' && arrayVerificarPunzada[2] == 'LHD') {
             alert('patron encontrado');
-            arrayVerificarPlumada = ['', '', ''];
-            arraySegundosVerificarPlumada = ['', '', ''];
+            arrayVerificarPunzada = ['', '', ''];
+            arraySegundosVerificarPunzada = ['', '', ''];
             return true;
         }
 
         if (value) {
-            //Verificamos errores
-            if (arrayVerificarPlumada[0] == 'LHU' || arrayVerificarPlumada[1] == 'LHD' || arrayVerificarPlumada[2] == 'LHU') {
-                arrayVerificarPlumada = ['', '', ''];
-                arraySegundosVerificarPlumada = ['', '', ''];
-            }
-
             //1. Primera posicion
             //Llenamos el arreglo cuando esta vacio
-            if (arrayVerificarPlumada[0] == '' && arrayVerificarPlumada[1] == '' && value == 'LHD') {
-                arrayVerificarPlumada[0] = 'LHD';
-                arraySegundosVerificarPlumada[0] = timeString
+            if (arrayVerificarPunzada[0] == '' && arrayVerificarPunzada[1] == '' && value == 'LHD') {
+                arrayVerificarPunzada[0] = 'LHD';
+                arraySegundosVerificarPunzada[0] = timeString
             }
 
             //Si encuentra un nuevo LHD, y no se ha llenado la siguiente posicion, actualizamos el tiempo
-            if (arrayVerificarPlumada[0] == 'LHD' && value == 'LHD' && arrayVerificarPlumada[1] == '') {
-                arraySegundosVerificarPlumada[0] = timeString
+            if (arrayVerificarPunzada[0] == 'LHD' && value == 'LHD' && arrayVerificarPunzada[1] == '') {
+                arraySegundosVerificarPunzada[0] = timeString
             }
 
             //2. Segunda posicion
-            if (arrayVerificarPlumada[1] == '' && arrayVerificarPlumada[2] == '' && value == 'LHU') {
-                arrayVerificarPlumada[1] = 'LHU';
-                arraySegundosVerificarPlumada[1] = timeString
+            if (arrayVerificarPunzada[1] == '' && arrayVerificarPunzada[2] == '' && arrayVerificarPunzada[0] == 'LHD' && value == 'LHU') {
+                arrayVerificarPunzada[1] = 'LHU';
+                arraySegundosVerificarPunzada[1] = timeString
             }
 
 
             //3. Tercera posicion
-            if (arrayVerificarPlumada[2] == '' && arrayVerificarPlumada[0] == 'LHD' && arrayVerificarPlumada[1] == 'LHU' && arrayVerificarPlumada[2] == '' && value == 'LHD') {
-                arrayVerificarPlumada[2] = 'LHD';
-                arraySegundosVerificarPlumada[2] = timeString;
+            if (arrayVerificarPunzada[2] == '' && arrayVerificarPunzada[0] == 'LHD' && arrayVerificarPunzada[1] == 'LHU' && arrayVerificarPunzada[2] == '' && value == 'LHD') {
+                arrayVerificarPunzada[2] = 'LHD';
+                arraySegundosVerificarPunzada[2] = timeString;
             }
         }
 
 
-        console.log(arraySegundosVerificarPlumada[0], arraySegundosVerificarPlumada[1], arraySegundosVerificarPlumada[2], '-', arrayVerificarPlumada[0], arrayVerificarPlumada[1], arrayVerificarPlumada[2]);
+        console.log(arraySegundosVerificarPunzada[0], arraySegundosVerificarPunzada[1], arraySegundosVerificarPunzada[2], '-', arrayVerificarPunzada[0], arrayVerificarPunzada[1], arrayVerificarPunzada[2]);
 
     }
 
@@ -80,7 +74,7 @@ export const PoseContextProvider = (props) => {
 
     return (
         <PoseContext.Provider value={{
-            checkPlumada: checkPlumada
+            checkPunzada: checkPunzada
 
         }}>
             {props.children}
