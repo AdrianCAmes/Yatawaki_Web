0ce6e38915f181366d97961a081446432d328287
Juan Carlos Hernández <39997847+Juanca0312@users.noreply.github.com>
2022-06-05 15:14:38 -0500

    ConfirmTradeDialog test

---
+++ src/test/pages/components/ConfirmTradeDialog.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { fireEvent, render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import ConfirmTradeDialog from '../../../pages/components/ConfirmTradeDialog'
+import UserUnlockableApi from '../../../apis/user-unlockable-apis'
+
+jest.mock("axios", () => {
+    return {
+        create: jest.fn(() => ({
+            interceptors: {
+                response: { use: jest.fn(), eject: jest.fn() }
+            },
+        }))
+    }
+});
+
+test('test trade', async () => {
+
+    const spy = jest.spyOn(UserUnlockableApi, 'trade')
+    .mockResolvedValue({
+        status: 200
+    })
+
+    const unlockable = {
+        description: 'Esta es la prueba de descripcion',
+        name: 'Esta es la prueba de name'
+    }
+
+    const component = render(
+        <BrowserRouter>
+            <ConfirmTradeDialog open={true} unlockable={unlockable}></ConfirmTradeDialog>
+        </BrowserRouter>
+    )
+
+    const aceptarButton = component.getByText('Aceptar').parentNode
+
+    fireEvent.click(aceptarButton)
+
+})
